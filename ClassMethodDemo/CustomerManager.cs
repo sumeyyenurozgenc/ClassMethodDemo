2dd433968880ab4046126bddd6bac96f65dc5bee
sumeyyenurozgenc <sumeyyenurozgenc@gmail.com>
2021-01-17 17:42:20 +0300

    Banka Müşteri Takip simülasyonu oluşturuldu. Temel olarak Customer(Customer'a ait alan adları Id, Tc, Ad...), CustomerManager(Ekleme, Silme, Listeleme) oluşturuldu.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add_Customer(Customer customer)
+        {
+            Console.WriteLine("Customer was added in the system");
+           
+        }
+
+        public void Get_All_Customer(Customer[] customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Customer: " + customer.Name + " " + customer.Surname);
+            }
+            Console.WriteLine();
+        }
+
+        public void Delete_Customer(int id)
+        {
+            Console.WriteLine("Customer was deleted.");
+        }
+    }
+}
--- ClassMethodDemo/Program.cs
+++ ClassMethodDemo/Program.cs
@@ -6,7 +6,33 @@ namespace ClassMethodDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Tc = "1234858423";
+            customer1.Name = "Nur";
+            customer1.Surname = "Özgenç";
+            customer1.AllAssets = 10045.323;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Tc = "4657687864";
+            customer2.Name = "Merve";
+            customer2.Surname = "Deniz";
+            customer2.AllAssets = 2000;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Tc = "8457687864";
+            customer3.Name = "Aytaç";
+            customer3.Surname = "Kaleci";
+            customer3.AllAssets = 212002.345;
+
+            Customer[] allCustomers = new Customer[] { customer1, customer2, customer3 };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Get_All_Customer(allCustomers);
+
+
         }
     }
 }
